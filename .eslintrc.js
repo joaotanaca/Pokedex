2aa22a11078a93a0c79d5d9bc4be88f3dfef18f3
João Ramos <joao.ramos@ies2.com.br>
2020-11-08 01:44:46 -0300

    Instalando eslint, prettier e configurando

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		'jest/globals': true,
+	},
+	extends: ['plugin:react/recommended', 'airbnb'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	plugins: ['react', '@typescript-eslint', 'jest'],
+	rules: {
+		'import/extensions': 'off',
+		'import/no-unresolved': 'off',
+		'react/jsx-filename-extension': 'off',
+		'no-use-before-define': 'off',
+		'jest/no-disabled-tests': 'warn',
+		'jest/no-focused-tests': 'error',
+		'jest/no-identical-title': 'error',
+		'jest/prefer-to-have-length': 'warn',
+		'jest/valid-expect': 'error',
+	},
+}
